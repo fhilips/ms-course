552d1b61c9214670cb7da17bd735bd2b459b201c
fhilips <fhilips.souza@gmail.com>
2021-10-17 13:06:56 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/filipe/hrworker/entities/Worker.java
@@ -0,0 +1,79 @@
+package com.filipe.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private Double dailyIncome;
+		
+	public Worker() {
+	}
+
+	public Worker(long id, String name, Double dailyIncome) {
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}	
+	
+		
+}
